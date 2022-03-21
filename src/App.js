d0b1392cb1e2947d4d479ee29742bb79f1463bbc
Shahbaaz <shahbaazc@gmail.com>
2022-03-21 15:12:41 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import Counter from './components/Counter';
-
+import { Fragment } from "react";
+import { useSelector } from "react-redux";
+import Auth from "./components/Auth";
+import Counter from "./components/Counter";
+import Header from "./components/Header";
 
 function App() {
+  const auth = useSelector((state) => state.auth.isAuthenticated);
   return (
-    <Counter />
+    <Fragment>
+      <Header/>
+      {!auth && <Auth />}
+      <Counter />
+    </Fragment>
   );
 }
 
