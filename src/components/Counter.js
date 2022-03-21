2abeb681937263cd42bff5aac63818981547c870
Shahbaaz <shahbaazc@gmail.com>
2022-03-21 15:12:04 +0530

    get states & dispatch data from components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,35 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { counterActions } from '../store/counters';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  // this "state.counter" will call from store index.js after  use state values
+  const value = useSelector((state) => state.counter.value); 
+  const show = useSelector((state) => state.counter.showCounter);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch(counterActions.increment())
+  }
+  const decrement = () => {
+    dispatch(counterActions.decrement())
+  }
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter())
+  };
+  const increase = () => {
+    // dispatch(counterActions.increase({payload_1: 10, payload_2: 20}))
+    dispatch(counterActions.increase(10))
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      {show && <div className={classes.value}>{value}</div>}
+      <button className={classes.btn_mar_10} onClick={increment}>Incremet</button>
+      <button className={classes.btn_mar_10}  onClick={decrement}>Decrement</button>
+      <button className={classes.btn_mar_10}  onClick={increase}>Increse by 10</button>
+      <button className={classes.btn_mar_10}  onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,31 @@
-import classes from './Header.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "../store/auth";
+import classes from "./Header.module.css";
 
 const Header = () => {
+  const auth = useSelector((state) => state.auth.isAuthenticated);
+  const dispatach = useDispatch();
+  const logOff = () => {
+    dispatach(authActions.logout());
+  };
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {auth && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={logOff}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
